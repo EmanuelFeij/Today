db5970704fff895a4a150d0fcc874ab0426be5e2
EmanuelFeij <emanuelfeijo93@gmail.com>
2021-06-16 21:55:14 +0100

    Added Kanye package, returns random tweets of Kanye West

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/EmanuelFeij/Today/weather"
+	"github.com/EmanuelFeij/Today/kanye"
 )
 
 func main() {
-	prevision, err := weather.GetWeather("CoImBra", true)
-	if err != nil {
-		return
-	}
-	fmt.Println(prevision)
+	fmt.Println(kanye.GetKanyeTweet())
 }
